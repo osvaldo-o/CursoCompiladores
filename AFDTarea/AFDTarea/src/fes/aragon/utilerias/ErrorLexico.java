f1380dafdcd27f4ee84c347c7be9cd22422f4d6d
osvaldo <osvaldo.guerra.c008@gmail.com>
2022-03-05 09:12:40 -0600

    act

--- AFDTarea/AFDTarea/src/fes/aragon/utilerias/ErrorLexico.java
+++
@@ -1,9 +0,0 @@
-package fes.aragon.utilerias;
-
-public class ErrorLexico extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public ErrorLexico(String msg) {
-		super(msg);
-	}
-}
